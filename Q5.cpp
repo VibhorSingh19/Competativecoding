1b295929d55565a77f312a655e9647d90e144a7a
Vibhor Singh <vibhor.formal@gmail.com>
2020-11-25 23:56:17 +0530

    Update Q5.cpp

--- Q5.cpp
+++ Q5.cpp
@@ -31,7 +31,14 @@ int main()
         {
             str1=str.substr(0,l/2);
          sort(str1.begin(), str1.end()); 
-         
+         str2=str.substr(l/2+1,l);
+         sort(str2.begin(), str2.end());
+         if(str1.compare(str2)==0)
+         {
+             cout<<"YES"<<endl;
+         }
+         else
+         cout<<"NO"<<endl;
          
         }
         
