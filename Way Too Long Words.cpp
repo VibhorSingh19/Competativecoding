ed72aac8b4fb3dd38838aee4aacb35d028ab27c9
Vibhor Singh <vibhor.formal@gmail.com>
2021-03-23 22:20:39 +0530

    Way too long string

--- Way Too Long Words.cpp
+++ Way Too Long Words.cpp
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include <bits/stdc++.h> 
+#include <string>
 using namespace std;
 
 int main()
@@ -11,14 +12,15 @@ int main()
     {
         string word="",newword="";
         cin>>word;
+        //getline(cin, word);
         int l=word.length();
         if(l>10)
         {
-        //newword=newword+word.at(0)+to_string(l-2)+word.at(l-1);
-        cout<<newword;
+        newword=newword+word.at(0)+to_string(l-2)+word.at(l-1);
+        cout<<newword<<endl;
         }
         else
-        cout<<word;
+        cout<<word<<endl;
     }
     return 0;
 }
---
+++ spiralmatrix.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    int arr[n][m];
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    int row_start=0,row_end=n-1,column_start=0,column_end=m-1;
+    while(row_start<row_end||column_start<column_end)
+    {
+        for(int i=column_start;i<=column_end;i++)
+        {
+            cout<<arr[row_start][i]<<"->";
+        }
+        row_start++;
+        for(int i=row_start;i<=row_end;i++)
+        {
+            cout<<arr[i][column_end]<<"->";
+        }
+        column_end--;
+        for(int i=column_end;i>=column_start;i--)
+        {
+            cout<<arr[row_end][i]<<"->";
+        }
+        row_end--;
+        for(int i=row_end;i>=row_start;i--)
+        {
+            cout<<arr[i][column_start]<<"->";
+        }
+        column_start++;
+    }
+    return 0;
+}
