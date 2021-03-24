bfd32159e40d562ad7bd886d0c973ad9bfc19f38
Vibhor Singh <vibhor.formal@gmail.com>
2021-03-24 23:59:14 +0530

    Domino piling updated

--- Domino piling.cpp
+++ Domino piling.cpp
@@ -5,25 +5,9 @@ using namespace std;
 
 int main()
 {
-    int N;
-    cin>>N;
-    int arr[3];
-    int c=0,c1=0;
-    for(int j=0;j<N;j++)
-    {
-    for(int i=0;i<3;i++)
-    {
-        cin>>arr[i];
-    }
-    for(int i=0;i<3;i++)
-    {
-        if(arr[i]==1)
-        c++;
-    }
-    if(c>=2)
-    c1++;
-    c=0;
-    }
-    cout<<c1<<endl;
+    int M,N;
+    cin>>M>>N;
+    int x=M*N;
+    cout<<(x/2)<<endl;
     return 0;
 }
